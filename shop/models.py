c6557ac27313deaa5648b5a626a65fcf2437901f
schojo137 <jonas.schopf@teilnehmer-bfwsb.de>
2023-11-21 15:05:55 +0100

    Migration DB

--- shop/models.py
+++ shop/models.py
@@ -1,3 +1,22 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+
+
+class Kunde(models.Model):
+
+    benutzer = models.OneToOneField(User, verbose_name=("Benutzer"), on_delete=models.RESTRICT, null=True, blank=True)
+    name = models.CharField("Name", max_length=200, null=True, blank=True)
+    email = models.CharField("E-Mail", max_length=200, null=True, blank=True)
+    
+    class Meta:
+        verbose_name = ("Kunde")
+        verbose_name_plural = ("Kunden")
+
+    def __str__(self):
+        return f"{self.benutzer}/{self.name}"
+
+    def get_absolute_url(self):
+        return reverse("Kunde_detail", kwargs={"pk": self.pk})
+
